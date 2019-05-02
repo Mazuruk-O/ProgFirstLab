eb65fc68c67b7d387ee5f35b67258aa43c3d92ae
Oleg <41341121+Mazuruk-O@users.noreply.github.com>
2019-05-02 08:46:26 +0300

    Update ComputationalMethods.java

--- src/ComputationalMethods/ComputationalMethods.java
+++ src/ComputationalMethods/ComputationalMethods.java
@@ -5,10 +5,11 @@ public class ComputationalMethods {
 
     public static double MethodNewton(double x, double eps, Function function)
     {
+        Function diffFunc = function.diff();
         double f=0.0, df=0.0; int iter = 0;
         do {
             f = function.calcFunc(x);
-            df = function.diff().calcFunc(x);
+            df = diffFunc.calcFunc(x);
             x = x - f / df;
             iter++;
         } while (Math.abs(f) > eps && iter < 20000);
